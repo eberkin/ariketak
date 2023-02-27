d6a6f7b1e43b5d1521e1fa4ad1025955f9d2cbcc
eberkin <equintana22@izarraitz.eus>
2023-02-27 14:15:39 +0100

    d

--- kontaktua.java
+++ kontaktua.java
@@ -1,6 +1,18 @@
 public class Kontaktua {
     public String izena;
     public int telefonoa;
+    public void setIzena(String izena) {
+        this.izena = izena;
+    }
+    public void setTelefonoa(int telefonoa) {
+        this.telefonoa = telefonoa;
+    }
+    public String getIzena() {
+        return izena;
+    }
+    public int getTelefonoa() {
+        return telefonoa;
+    }
     
 
 }
