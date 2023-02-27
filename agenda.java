b2ac301ad58c340b8db166dfbbc53e80b08df09a
eberkin <equintana22@izarraitz.eus>
2023-02-27 13:59:03 +0100

    s

--- agenda.java
+++ agenda.java
@@ -1,7 +1,18 @@
 
 public class agenda{
 
-
+public void KontaktuakGehitu() {
+    
+}
+public boolean KontaktuaExistitzenDu(Kontaktua k) {
+    
+}
+public void KontaktuakZerrendatu() {
+    
+}
+public void KontataktuaBilatu(String izena) {
+    
+}
 public static void main(String[] args) {
     
 }
