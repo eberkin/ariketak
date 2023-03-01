2b2ea41d7a5718b772ba82374b13a5bd4a1d4d1f
eberkin <equintana22@izarraitz.eus>
2023-03-01 10:07:59 +0100

    ee

--- agenda.java
+++ agenda.java
@@ -26,6 +26,29 @@ public void Hutsuneak() {
     
 }
 public static void main(String[] args) {
-    System.out.println();
+    
+    Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Aukeratu ekintza:");
+            System.out.println("1. Kontaktua gehitu");
+            System.out.println("2. Kontaktua bilatu");
+            System.out.println("3. Kontaktua ezabatu");
+            System.out.println("4. Agendako kontaktuak zerrendatu");
+            System.out.println("5. Hutsuneak erakutsi");
+            System.out.println("6. Agenda beteta dagoen edo ez");
+            System.out.println("0. Irten");
+            int aukera = scanner.nextInt();
+
+            switch(aukera){
+                case 0:
+                System.out.println("Agur!");
+                scanner.close();
+                System.exit(0);
+                break;
+                case 1:
+                System.out.println("Sartu kontaktuaren izena");
+                
+            }
+        }
 }
 }
