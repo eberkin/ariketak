9dd9e2a99a3bbaae21b44361e9709f8167279c85
eberkin <equintana22@izarraitz.eus>
2023-03-14 10:45:53 +0100

    ss

--- a4/Autoa.java
+++ a4/Autoa.java
@@ -1,5 +1,47 @@
 package a4;
 
+import java.time.LocalDate;
+
 public class Autoa {
-    
+  public String matrikula;
+  public LocalDate salmentaData;
+  public String modeloa;
+  public String marka;
+
+
+public Autoa(String matrikula, LocalDate salmentaData, String modeloa, String marka){
+    this.matrikula = matrikula;
+    this.marka = marka;
+    this.modeloa = modeloa;
+    this.salmentaData = salmentaData;
+}
+public String getMatrikula() {
+    return matrikula;
+}
+public String getModeloa() {
+    return modeloa;
+}
+public LocalDate getSalmentaData(){
+    return salmentaData;
+}
+public String getMarka() {
+    return marka;
+}
+public boolean bermeanDago() {
+    LocalDate orain = LocalDate.now();
+    return salmentaData.plusYears(2).isAfter(orain);
+}
+public String toString() {
+    return matrikula + " " + modeloa + " " + marka + " " + salmentaData.toString();
+}
+public boolean equals(Object o) {
+    if (o == null) {
+        return false;
+    }
+    if (getClass() != o.getClass()) {
+        return false;
+    }
+    Autoa a = (Autoa) o;
+    return matrikula.equals(a.matrikula);
+}
 }
